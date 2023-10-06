1fff37be3a0bb54fc40733df2a52a5cbbf07802d
mrcotrmpr <marco1088@hotmail.nl>
2023-10-06 13:56:41 +0200

    setup sdl

--- src/main.cpp
+++ src/main.cpp
@@ -1,8 +1,42 @@
+#define SDL_MAIN_HANDLED
+#include <SDL.h>
 #include "domain/console_ui.hpp"
 
 int main() {
+    //ConsoleUI app;
+    //app.start();
 
-    ConsoleUI app;
-    app.start();
+    if (SDL_Init(SDL_INIT_VIDEO) < 0) {
+        SDL_Log("SDL initialization failed: %s", SDL_GetError());
+        return 1;
+    }
+
+    SDL_Window* window = SDL_CreateWindow("Museum", SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED, 800, 800, SDL_WINDOW_SHOWN);
+    if (!window) {
+        SDL_Log("Failed to create window: %s", SDL_GetError());
+        return 1;
+    }
+
+    SDL_Renderer* renderer = SDL_CreateRenderer(window, -1, SDL_RENDERER_ACCELERATED | SDL_RENDERER_PRESENTVSYNC);
+    if (!renderer) {
+        SDL_Log("Failed to create renderer: %s", SDL_GetError());
+        return 1;
+    }
+
+    bool quit = false;
+    while (!quit) {
+        SDL_Event event;
+        while (SDL_PollEvent(&event)) {
+            if (event.type == SDL_QUIT) {
+                quit = true;
+            }
+        }
+        SDL_RenderPresent(renderer);
+    }
+
+    SDL_DestroyRenderer(renderer);
+    SDL_DestroyWindow(window);
+    SDL_Quit();
+
     return 0;
 }
